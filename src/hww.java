682d7a9620392debffaf3485a077c07aac072e84
CheesyCheese60 <111364480+CheesyCheese60@users.noreply.github.com>
2025-12-16 10:48:33 +0000

    moved the adding to a function called add

--- src/hww.java
+++ src/hww.java
@@ -6,16 +6,20 @@ public class hww {
         StudentAmount = scanner.nextInt();
         scanner.nextLine(); // this has to be here for some reason??
         String base[][] = new String[StudentAmount][2];
-        System.out.println("enter the student ids");
-        for (int i = 0; i < base.length; i++) {
-            base[i][0]=scanner.nextLine();
-        }
-        System.out.println("now enter the names");
-        for (int i = 0; i < base.length; i++) {
-            base[i][1]=scanner.nextLine();
-        } 
+        add(base); 
         for (int i = 0; i < StudentAmount; i++) {
             System.out.println("["+ (i + 1) + "] student id " + base[i][0] + " their name is " + base[i][1]);
+        }
+    }
+    static void add(String b[][]){
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.println("enter the student ids");
+        for (int i = 0; i < b.length; i++) {
+            b[i][0]=scanner.nextLine();
+        }
+        System.out.println("now enter the names");
+        for (int i = 0; i < b.length; i++) {
+            b[i][1]=scanner.nextLine();
         }
     }
 }
