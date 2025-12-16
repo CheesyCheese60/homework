ad346b6f7c899046566ec36098f2cc8e196a27ab
CheesyCheese60 <111364480+CheesyCheese60@users.noreply.github.com>
2025-12-16 11:19:45 +0000

    did soem stuff gtg

--- src/hww.java
+++ src/hww.java
@@ -6,12 +6,43 @@ public class hww {
         StudentAmount = scanner.nextInt();
         scanner.nextLine(); // this has to be here for some reason??
         String base[][] = new String[StudentAmount][2];
-        add(base); 
-        for (int i = 0; i < StudentAmount; i++) {
-            System.out.println("["+ (i + 1) + "] student id " + base[i][0] + " their name is " + base[i][1]);
+//        addall(base); 
+        view(base); 
+        while (true) {
+            System.out.println("Student Management System\n1. to add student type \"add\" \nto leave type \"exit\"");
+            String command = scanner.nextLine();
+            if(command == "add"){
+                add(base);
+            }
+            else if(command == "exit"){
+                break;
+            }
+            else{
+                System.out.println("invalid command");
+            }
+            view(base);
+        }
+   }
+    static void view(String b[][]){
+        for (int i = 0; i < b.length; i++) {
+            
+                System.out.println("student id: " + b[i][0] + " name: " + b[i][1]);
+            
         }
     }
     static void add(String b[][]){
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        for (int i = 0; i < b.length; i++) {
+            if (b[i][0]==null) {
+                System.out.println("enter the student id:");
+                b[i][0]=scanner.nextLine();
+                System.out.println("enter the name id:");
+                b[i][1]=scanner.nextLine();
+                break;
+            }
+        }
+    }
+    static void addall(String b[][]){
         java.util.Scanner scanner = new java.util.Scanner(System.in);
         System.out.println("enter the student ids");
         for (int i = 0; i < b.length; i++) {
